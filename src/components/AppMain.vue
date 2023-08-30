fca9196dd670686a4a20b2d18f63689b9f309d4d
Marco-Vigilio <tonyvigilio@gmail.com>
2023-08-30 19:08:48 +0200

    create vite-boolfolio

---
+++ src/components/AppMain.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Main</h1>
+    </div>
+    <div>
+        <p v-for="project in projects">
+            {{ project.title }}
+        </p>
+    </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+    name: "AppMain",
+
+    data() {
+        return {
+            projects: [],
+            apiUrl: "http://127.0.0.1:8000/api/projects",
+        }
+    },
+
+    methods: {
+        getProjects() {
+            //recupero projects e popolo le variabili
+            axios.get(this.apiUrl, {
+                params: {}
+            })
+                .then((response) => {
+                    //console.log(response.data);
+                    this.projects = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+    created() {
+        this.getProjects();
+    }
+}
+</script>
+<style lang="scss"></style>
