04b604af6be211970be2275c3613ed043cde5826
Marco-Vigilio <tonyvigilio@gmail.com>
2023-08-31 15:33:14 +0200

    insert route-view in App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,9 @@ export default {
 
 <template>
   <!--
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  -->
   <AppMain />
+  -->
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
