1a796316f2ff63bbde5fb1bc00518224a41591c8
Marco-Vigilio <tonyvigilio@gmail.com>
2023-08-31 18:59:42 +0200

    show api in Home

--- src/components/pages/AppHome.vue
+++ src/components/pages/AppHome.vue
@@ -1,12 +1,45 @@
 <script>
+import axios from "axios";
+import SingleProjects from '../pages/SingleProjects.vue';
+
+
 export default {
     name: "AppHome",
+    components: {
+        SingleProjects
+    },
+    data() {
+        return {
+            projects: [],
+            apiUrl: "http://127.0.0.1:8000/api/projects",
+        }
+    },
+    methods: {
+        getProjects() {
+            //recupero projects e popolo le variabili
+            axios.get(this.apiUrl, {
+                params: {}
+            })
+                .then((response) => {
+                    console.log(response.data);
+                    this.projects = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+    created() {
+        this.getProjects();
+    }
 }
 </script>
 
 <template>
     <section>
         <h1>Home</h1>
+        <SingleProjects v-if="projects" :projects='projects' />
     </section>
 </template>
 <style lang="scss"></style>
---
+++ src/components/pages/SingleProjects.vue
@@ -0,0 +1,44 @@
+<template lang="">
+    <article class="single-post w-100">
+        <h2>
+            {{ projects.title }}
+        </h2>
+    </article>
+</template>
+<script>
+export default {
+    name: 'SingleProject',
+    props: {
+        'projects': Object,
+    }
+}
+</script>
+<style lang="scss" scoped>
+.single-post {
+    width: calc((100% / 2) - 1rem);
+    border-radius: 1rem;
+    padding: 1rem;
+    margin-right: 1rem;
+    background-color: rgb(221, 221, 221);
+    color: black;
+    margin-bottom: 2rem;
+
+    * {
+        margin-bottom: 1rem;
+    }
+
+    img {
+        width: 100%;
+        height: 10rem;
+        object-fit: cover;
+    }
+
+    h6 span {
+        margin-right: 1rem;
+    }
+
+    &:hover {
+        background-color: white;
+    }
+}
+</style>
